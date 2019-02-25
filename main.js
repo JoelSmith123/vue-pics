39a203491a2ed899b5802a229cbad919eda3417e
Joel Smith <joelwsmith123@gmail.com>
2019-02-25 14:32:16 -0700

    Add selected img to main img display

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
     apiKey: '7193c4218fce9d20f4890f70c6b4fbe51e036dcbc70b5bc70732bd7c85dae0af',
     searchField: '',
     imagesArr: [],
+    selectedImg: ''
   },
   methods: {
     submitSearch: function () {
@@ -12,5 +13,9 @@ var app = new Vue({
         .then(images => this.imagesArr = images.results)
         .catch(error => console.log(error))
     },
+    onCollectionImgClick: function (event) {
+      const imgSrc = event.target.src.replace('&h=100', '')
+      this.selectedImg = imgSrc
+    }
   }
 })
