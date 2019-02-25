1535f008cb496979353f62749f78b07e0220c677
Joel Smith <joelwsmith123@gmail.com>
2019-02-25 13:38:28 -0700

    Add search input for images fetch

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ var app = new Vue({
   el: '.search-form',
   data: {
     apiKey: '7193c4218fce9d20f4890f70c6b4fbe51e036dcbc70b5bc70732bd7c85dae0af',
-    search: 'kittens',
+    searchField: '',
     imagesArr: [],
   },
   methods: {
     submitSearch: function () {
-      fetch(`https://api.unsplash.com/search/photos?query=${this.search}&client_id=${this.apiKey}`)
+      fetch(`https://api.unsplash.com/search/photos?query=${this.searchField}&client_id=${this.apiKey}`)
         .then(response => response.json())
         .then(images => this.imagesArr = images.results)
         .catch(error => console.log(error))
