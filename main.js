fa086e648314b07564ee937dcdd96705db468da7
Joel Smith <joelwsmith123@gmail.com>
2019-02-26 19:15:01 -0700

    Add conditional rendering to Vue

--- main.js
+++ main.js
@@ -3,19 +3,37 @@ var app = new Vue({
   data: {
     apiKey: '7193c4218fce9d20f4890f70c6b4fbe51e036dcbc70b5bc70732bd7c85dae0af',
     searchField: '',
+    searchFieldDisplay: '',
     imagesArr: [],
-    selectedImg: ''
+    selectedImg: '',
+    images: false
   },
+  mounted: function () {
+    this.searchFieldDisplay = 'Micro Worlds'
+    let url = `https://api.unsplash.com/collections/573009/photos?client_id=${this.apiKey}`
+    this.fetchImagesFromApi(url)
+  },
+
   methods: {
     submitSearch: function () {
-      fetch(`https://api.unsplash.com/search/photos?query=${this.searchField}&client_id=${this.apiKey}`)
-        .then(response => response.json())
-        .then(images => this.imagesArr = images.results)
-        .catch(error => console.log(error))
+      this.images = true
+      let url = `https://api.unsplash.com/search/photos?query=${this.searchField}&client_id=${this.apiKey}`
+      this.fetchImagesFromApi(url)
+      this.searchFieldDisplay = this.searchField
+      this.searchField = ''
     },
+
     onCollectionImgClick: function (event) {
       const imgSrc = event.target.src.replace('&h=100', '')
       this.selectedImg = imgSrc
+    },
+
+    fetchImagesFromApi: function (url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(images => images.results ? this.imagesArr = images.results : this.imagesArr = images)
+        .then(() => this.selectedImg = this.imagesArr[0].urls.raw)
+        .catch(error => console.log(error))     
     }
   }
 })
